5cbe4de4ee9303b617311a21c1968f675675b0c4
Roma <romamarusyk@gmail.com>
2015-04-08 22:56:56 +0300

    Bridge

---
+++ PatternsProjects/Bridge/ConcreteImplementorA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class ConcreteImplementorA : Implementor
+    {
+        public override void OperationImp()
+        {
+            Console.WriteLine("ImplementorA");
+        }
+    }
+}
