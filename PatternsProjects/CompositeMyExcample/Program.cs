14e8bc550423e1cca4e5b7ec115623954d5b1da3
Roma <romamarusyk@gmail.com>
2015-04-11 00:46:58 +0300

    Composite Excample

---
+++ PatternsProjects/CompositeMyExcample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeMyExcample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildCountry();
+            Console.ReadKey();
+        }
+
+        public static void BuildCountry()
+        {
+            Country lviv = new Region("Lvivska");
+            Country kyiv = new Region("Kyivska");
+            Country ukraine = new Region("Ukraine");
+            Country Lviv = new City("Lviv");
+            Country Kyiv = new City("Kyivn");
+
+            ukraine.Add(lviv);
+            ukraine.Add(kyiv);
+            lviv.Add(Lviv);
+            kyiv.Add(Kyiv);
+            ukraine.ShowName();
+        }
+    }
+}
---
+++ PatternsProjects/CompositeMyExcample/Region.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeMyExcample
+{
+    class Region : Country
+    {
+        ArrayList node;
+
+        public Region(string name)
+            :base(name)
+        {
+            node = new ArrayList();
+        }
+
+        public override void Add(Country element)
+        {
+            node.Add(element);
+        }
+
+        public override void Remove(Country element)
+        {
+            node.Remove(element);
+        }
+
+        public override Country GetChild(int index)
+        {
+            return node[index] as Country;
+        }
+
+        public override void ShowName()
+        {
+            Console.WriteLine(name);
+
+            foreach (Country el in node)
+                el.ShowName();
+        }
+    }
+}
