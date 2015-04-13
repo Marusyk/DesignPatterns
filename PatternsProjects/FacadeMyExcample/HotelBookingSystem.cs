def60dec1f99188157467cdd769ad5150a29f0f4
Roma <romamarusyk@gmail.com>
2015-04-13 21:28:36 +0300

    Facade MyExcample

---
+++ PatternsProjects/FacadeMyExcample/HotelBookingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeMyExcample
+{
+    class HotelBookingSystem
+    {
+        public int BookRoom(int roomQuality)
+        {
+            switch (roomQuality)
+            {
+                case 3:
+                    return 250;
+                case 4:
+                    return 500;
+                case 5:
+                    return 900;
+                default:
+                    throw new ArgumentException("roomQuality should be in range [3;5]");
+            }
+        }
+    }
+}
---
+++ PatternsProjects/FacadeMyExcample/SkiRent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeMyExcample
+{
+    class SkiRent
+    {
+        public int RentBoots(int feetSize, int skierLevel)
+        {
+            return 20;
+        }
+
+        public int RentPole(int height)
+        {
+            return 5;
+        }
+
+        public int RentSki(int weight, int skierLevel)
+        {
+            return 40;
+        }
+    }
+}
