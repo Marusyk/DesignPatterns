9e5b62b6fb65395728247483066d0075e24e7df4
Roma <romamarusyk@gmail.com>
2015-04-14 18:52:31 +0300

    Flyweight

---
+++ PatternsProjects/Flyweight/ConcreteFlyweight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class ConcreteFlyweight : Flyweight
+    {
+        int intrinsicState;
+
+        public override void Operation(int extrinsicState)
+        {
+            intrinsicState = extrinsicState;
+        }
+    }
+}
---
+++ PatternsProjects/Flyweight/UnsharedConcreteFlyweight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    class UnsharedConcreteFlyweight : Flyweight
+    {
+        int allState;
+
+        public override void Operation(int extrinsicState)
+        {
+            allState = extrinsicState;
+        }
+    }
+}
