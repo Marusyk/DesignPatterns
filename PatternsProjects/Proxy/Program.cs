17b5e3837149ba22422e46b486407d3bc4d6c428
Roma <romamarusyk@gmail.com>
2015-04-15 23:33:29 +0300

    Proxy

---
+++ PatternsProjects/Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Proxy();
+            subject.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PatternsProjects/Proxy/Proxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Proxy : Subject
+    {
+        RealSubject realSubject;
+
+        public override void Request()
+        {
+            if (realSubject == null)
+                realSubject = new RealSubject();
+
+            realSubject.Request();
+        }
+    }
+}
