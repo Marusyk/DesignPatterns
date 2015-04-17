e3ba0fe2769a8c1e76fd6ab60adfaf8c8f781a0b
Roma <romamarusyk@gmail.com>
2015-04-17 23:33:02 +0300

    ChainOfResponsibility

---
+++ PatternsProjects/ChainOfResponsibility/Handler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    abstract class Handler
+    {
+        public Handler Successor { get; set; }
+        public abstract void HandleRequest(int request);
+    }
+}
