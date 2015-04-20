2264d7eea786cae248af6fd48f8369a96c129e71
Roma <romamarusyk@gmail.com>
2015-04-21 00:57:19 +0300

    Command excample

---
+++ PatternsProjects/CommandMyExcample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandMyExcample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            int result = 0;
+
+            // 0 + 5 = 5
+            result = calculator.Add(5);
+            Console.WriteLine(result);
+
+            // 5 - 3 = 2
+            result = calculator.Sub(3);
+            Console.WriteLine(result);
+
+            // undo 0 + 5 = 0
+            result = calculator.Undo(2);
+            Console.WriteLine(result);
+
+            // redo 0 + 5 = 0 -> 0 + 5 = 5
+           // result = calculator.Redo(1);
+           // Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PatternsProjects/CommandMyExcample/Sub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandMyExcample
+{
+    class Sub : Command
+    {
+        public Sub(ArithmeticUnit unit, int operand)
+        {
+            this.unit = unit;
+            this.operand = operand;
+        }
+
+        public override void Execute()
+        {
+            unit.Run('-', operand);
+        }
+
+        public override void UnExecute()
+        {
+            unit.Run('+', operand);
+        }
+    }
+}
