7d0b9d718c14c2b07f390ccfd88c777d5f4f9d74
Roma <romamarusyk@gmail.com>
2015-04-21 00:04:27 +0300

    Command

---
+++ PatternsProjects/Command/Invoker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    class Invoker
+    {
+        Command command;
+
+        public void StoreCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            command.Execute();
+        }
+    }
+}
