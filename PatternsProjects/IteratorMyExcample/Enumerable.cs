0cb02f95bedbc84c5fb9fb5dcc2e19fe2a10adb4
Roma <romamarusyk@gmail.com>
2015-04-27 22:30:01 +0300

    Iterator MyExcample1

---
+++ PatternsProjects/IteratorMyExcample/Enumerable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorMyExcample
+{
+    class Enumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return "Element";
+        }
+    }
+}
---
+++ PatternsProjects/IteratorMyExcample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorMyExcample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable en = new Enumerable();
+
+            foreach (var item in en)
+                Console.WriteLine(item);
+        }
+    }
+}
