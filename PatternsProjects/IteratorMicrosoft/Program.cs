90bbcab4ce168221e039337c55fcca097c3aeec9
Roma <romamarusyk@gmail.com>
2015-04-27 22:22:29 +0300

    Iterator Microsoft

---
+++ PatternsProjects/IteratorMicrosoft/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IteratorMicrosoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable a = new Enumerable();
+
+            a[0] = "Element A";
+            a[1] = "Element B";
+            a[2] = "Element C";
+            a[3] = "Element D";
+
+            foreach(var item in a)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
