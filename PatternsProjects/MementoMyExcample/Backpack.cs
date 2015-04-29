def2e8929268aa6bc85a11b1b131c18e6b369c98
Roma <romamarusyk@gmail.com>
2015-04-29 22:08:45 +0300

    Memento MyExcample. Wide and narrow interface

---
+++ PatternsProjects/MementoMyExcample/Backpack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoMyExcample
+{
+    class Backpack : IWideInterface
+    {
+        private string state;
+
+        public string GetState()
+        {
+            return state;
+        }
+
+        public void SetState(string s)
+        {
+            state = s;
+        }
+
+        public Backpack(string state)
+        {
+            this.state = state;
+        }
+    }
+}
