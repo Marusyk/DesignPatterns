050f4415e4a7e13577906eca9b052eb46e41c394
Roma <romamarusyk@gmail.com>
2015-05-02 16:09:08 +0300

    Observer(Push)

---
+++ PatternsProjects/Observer(Push)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Push_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteSubject subject = new ConcreteSubject();
+            subject.Attach(new ConcreteObserver(subject));
+            subject.Attach(new ConcreteObserver(subject));
+            subject.State = "Some State...";
+            subject.Notify();
+
+            Console.ReadKey();
+        }
+    }
+}
