e274a42c0776c6e98b24e85ff460fe0bc0cce0cd
Roma <romamarusyk@gmail.com>
2015-05-03 18:46:11 +0300

    State myExample

---
+++ PatternsProjects/StateMyExcample/Program.cs
@@ -0,0 +1,27 @@
+﻿using State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMyExcample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var father = new Father();
+
+            while (true)
+            {
+                Mark mark = (Mark)Enum.Parse(typeof(Mark), Console.ReadLine());
+
+                if (mark == Mark.Two || mark == Mark.Five)
+                    father.FindOut(mark);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
