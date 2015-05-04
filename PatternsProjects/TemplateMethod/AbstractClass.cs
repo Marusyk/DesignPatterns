849b20462aa5db04949c48c4e147f047aa8469a6
Roma <romamarusyk@gmail.com>
2015-05-04 23:19:03 +0300

    TemplateMethod

---
+++ PatternsProjects/TemplateMethod/AbstractClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+        }
+    }
+}
