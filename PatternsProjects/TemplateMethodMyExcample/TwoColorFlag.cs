40356f41eea6ed42d8b3fb4aaeecd17d9d37c584
Roma <romamarusyk@gmail.com>
2015-05-04 23:26:29 +0300

    TemplateMethod My Excample

---
+++ PatternsProjects/TemplateMethodMyExcample/TwoColorFlag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodMyExcample
+{
+    abstract class TwoColorFlag
+    {
+        public void Draw()
+        {
+            DrawTopPart();
+            DrawBottomPart();
+        }
+
+        protected abstract void DrawTopPart();
+        protected abstract void DrawBottomPart();
+    }
+}
---
+++ PatternsProjects/TemplateMethodMyExcample/UkraineFlag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodMyExcample
+{
+    class UkraineFlag : TwoColorFlag
+    {
+        protected override void DrawBottomPart()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(new string(' ', 7));
+        }
+
+        protected override void DrawTopPart()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(new string(' ', 7));
+        }
+    }
+}
